99a205c75b50ad5c78cf16140c3d12ff35eed6a4
Edoarda <medoarda@id.uff.br>
2016-01-13 15:28:43 -0200

    coisas

--- DojoHash/encadeamento_exterior.c
+++ DojoHash/encadeamento_exterior.c
@@ -7,6 +7,10 @@
 #include <stdio.h>
 
 #include "encadeamento_exterior.h"
+#include "compartimento_hash.h"
+#include "lista_compartimentos.h"
+#include "lista_clientes.h"
+#include "cliente.h"
 
 
 
@@ -22,40 +26,95 @@ void cria_hash(char *nome_arquivo_hash, int tam)
         printf("deu xibu");
 	}
 	fclose(arquivo);
-    printf("\n\n\n\n\n");
 }
 
 int busca(int cod_cli, char *nome_arquivo_hash, char *nome_arquivo_dados)
 {
-	//TODO: Inserir aqui o codigo do algoritmo
+    int h = cod_cli % 7;
+    ListaClientes *lisCli = le_clientes(nome_arquivo_dados);
+    int quant = lisCli->qtd;
+    int i;
+    for(i = 0; i < quant; i++){
+        Cliente *item = lisCli->lista[i];
+        if(item->cod_cliente == cod_cli){
+            if(item->flag == 1){
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+
+int insere(int cod_cli, char *nome_cli, char *nome_arquivo_hash, char *nome_arquivo_dados, int num_registros)
+{
+    Cliente *cli = cliente(cod_cli, nome_cli, -1, 1);
     int h = cod_cli % 7;
 
     FILE *arquivo = fopen(nome_arquivo_hash, "rb");
-    FILE *arquivoDados = fopen(nome_arquivo_dados, "rb");
-	if(arquivo != NULL && arquivoDados != NULL){
-        fseek(arquivo, h*sizeof(int));
-        CompartimentoHash compH = le_compartimento(arquivo);
-        fseek(arquivoDados, compH->prox);
-        ListaCompartimentos compLista = le_compartimentos(arquivoDados);
+	if(arquivo != NULL){
+        fseek(arquivo, (h * sizeof(int)), SEEK_SET);
+        ListaClientes *lisCli = le_clientes(nome_arquivo_dados);
+        int quant = lisCli->qtd;
+        int i;
+        if(quant == 0){
+            lisCli = cria_clientes(1, cli);
+            salva_clientes(nome_arquivo_dados, lisCli);
 
+                CompartimentoHash *cp = compartimento_hash(0);
+                salva_compartimento(compartimento_hash(cp), arquivo);
+            return 0;
+        }
+
+        for(i = 0; i < quant; i++){
+            Cliente *item = lisCli->lista[i];
+            if(item->flag == 0){
+                item = cli;
+                return i;
+            }
+        }
+        return -1;
 	}else{
         printf("deu xibu");
 	}
 	fclose(arquivo);
 
 
-    return INT_MAX;
-}
+
+    /*ListaClientes *lisCli = le_clientes(nome_arquivo_dados);
+    if(lisCli->lista[h] == -1){
+        lisCli->lista[h] = cli;
+        ListaClientes *lc = cria_clientes(num_registros, cli);
+        salva_clientes(nome_arquivo_dados, lc);
+        return 1;
+    }*/
 
 
-int insere(int cod_cli, char *nome_cli, char *nome_arquivo_hash, char *nome_arquivo_dados, int num_registros)
-{
-	//TODO: Inserir aqui o codigo do algoritmo de insercao
+    return 1;
+
+
     return INT_MAX;
 }
 
 int exclui(int cod_cli, char *nome_arquivo_hash, char *nome_arquivo_dados)
 {
-	//TODO: Inserir aqui o codigo do algoritmo de remocao
-    return INT_MAX;
+	int h = cod_cli % 7;
+    ListaClientes *lisCli = le_clientes(nome_arquivo_dados);
+    int quant = lisCli->qtd;
+    int i;
+    for(i = 0; i < quant; i++){
+        Cliente *item = lisCli->lista[i];
+        Cliente *itemA = lisCli->lista[i-1];
+        if(item->cod_cliente == cod_cli){
+            if(item->flag == 1){
+                item->flag = 0;
+                if(item->prox == -1){
+                    itemA->prox = -1;
+                }
+                salva_clientes(nome_arquivo_dados, lisCli);
+                return i;
+            }
+        }
+    }
+    return -1;
 }
